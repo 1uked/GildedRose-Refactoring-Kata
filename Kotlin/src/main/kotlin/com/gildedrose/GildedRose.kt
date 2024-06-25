71f1e04a646b54fb4b7463eb4c51b63b7bb59483
Luke Edwards <cm22683@bristol.ac.uk>
2024-06-25 16:12:31 +0100

    Variable Backstage Pass

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -19,18 +19,19 @@ class GildedRose(var items: List<Item>) {
     }
 
     //TODO Fix to set to Starts With -> Works with any ticket
-    //TODO Swap Coerce to Max/Min
     fun updateQualityValue(i: Item) : Int {
 
         var conjured = 1
         if (i.name.contains("Conjured")){ conjured = 2 }
+
+        if (i.name.contains("Backstage pass")) {i.name = "Backstage pass"}
 
         return when(i.name) {
             "Sulfuras, Hand of Ragnaros" -> i.quality
 
             "Aged Brie"  -> {(i.quality + 1).coerceAtMost(50)}
 
-            "Backstage passes to a TAFKAL80ETC concert" -> {
+            "Backstage pass" -> {
                 if (i.sellIn == 0) {0}
                 else if (i.sellIn <= 5) {(i.quality+3).coerceAtMost(50)}
                 else if (i.sellIn <= 10) {(i.quality+2).coerceAtMost(50)}
