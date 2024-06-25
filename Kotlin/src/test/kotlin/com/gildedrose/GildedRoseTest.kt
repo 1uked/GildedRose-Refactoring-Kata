63a3d2216c41519069b438380cc394797872c530
Luke Edwards <cm22683@bristol.ac.uk>
2024-06-25 12:08:13 +0100

    Initial Tests

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,12 +6,40 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
+    fun updateQuality_isSulfuras_Equal() {
+        val items = listOf(Item("Sulfuras, Hand of Ragnaros", 10, 80))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals(10, app.items[0].sellIn)
+        assertEquals(80, app.items[0].quality)
+    }
 
+    @Test
+    fun updateQuality_agedBrie_Increase() {
+        val items = listOf(Item("Aged Brie", 3, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(11, app.items[0].quality)
+        assertEquals(2, app.items[0].sellIn)
+    }
+
+
+    @Test
+    fun updateQuality_normalItem_Decrease() {
+        val items = listOf(Item("Cake", 5, 8))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(7, app.items[0].quality)
+        assertEquals(4, app.items[0].sellIn)
+    }
+
+    @Test
+    fun updateQuality_sellIn_equalsZero() {
+        val items = listOf(Item("Fruit", 0, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(8, app.items[0].quality)
+        assertEquals(-1, app.items[0].sellIn)
     }
 
 }
