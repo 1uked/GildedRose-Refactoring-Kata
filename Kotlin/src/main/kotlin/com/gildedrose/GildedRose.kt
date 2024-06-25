e843b0361af84a42fb93debd575d4d6569f5cab0
Luke Edwards <cm22683@bristol.ac.uk>
2024-06-25 15:44:51 +0100

    Code Refactor - Remove Original Code Refactor (Untested)

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,62 +1,10 @@
 package com.gildedrose
 
+import java.security.KeyStore.TrustedCertificateEntry
+
 class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-//            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-//                items[i].sellIn = items[i].sellIn - 1
-//            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
-        }
-    }
-
-
-
-    fun updateQuality_new() {
         items.forEachIndexed { index, element ->
             items[index].sellIn = updateSellInValue(element)
             items[index].quality = updateQualityValue(element)
@@ -72,20 +20,21 @@ class GildedRose(var items: List<Item>) {
         }
     }
 
+    //TODO Fix to set to Starts With -> Works with any ticket
+    //TODO Swap Coerce to Max/Min
     fun updateQualityValue(i: Item) : Int {
 
         return when(i.name) {
             "Sulfuras, Hand of Ragnaros" -> i.quality
 
-            "Aged Brie"  -> {
-                if (i.quality < 50) {
-                    i.quality + 1
-                }
-                i.quality
+            "Aged Brie"  -> {(i.quality + 1).coerceAtMost(50)}
+
+            "Backstage passes to a TAFKAL80ETC concert" -> {
+                if (i.sellIn == 0) {0}
+                else if (i.sellIn <= 5) {(i.quality+3).coerceAtMost(50)}
+                else if (i.sellIn <= 10) {(i.quality+2).coerceAtMost(50)}
+                else {(i.quality + 1).coerceAtMost(50)}
             }
-
-            //contains("Backstage") ->
-
 
             else -> {
                 if (i.sellIn < 0) {
@@ -95,9 +44,6 @@ class GildedRose(var items: List<Item>) {
                 }
             }
         }
-
-
-        return 0
     }
 
 
